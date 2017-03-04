20048d5772465569103e189591fae73df9ba693e
Yanzhishang <1176377464@qq.com>
2017-03-05 00:15:53 +0800

    Initial commit

---
+++ app/src/main/java/com/yzs/zx/uav/Setting.java
@@ -0,0 +1,16 @@
+package com.yzs.zx.uav;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZS on 17-3-3.
+ */
+
+public class Setting extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
